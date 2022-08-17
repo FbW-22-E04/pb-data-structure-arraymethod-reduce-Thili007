2049460bdaf59d3517fdb10d944bb74d23ed1217
Your Name <tpunchihewage@gmail.com>
2022-08-17 15:57:00 +0200

    added js files

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-
 //Create a function groupById(arr) that creates an object from it, with id as the key, and array items as values.
 //1
 
 let users = [
-  {id: 'john', name: "John Smith", age: 20},
-  {id: 'ann', name: "Ann Smith", age: 24},
-  {id: 'pete', name: "Pete Peterson", age: 31},
+  { id: "john", name: "John Smith", age: 20 },
+  { id: "ann", name: "Ann Smith", age: 24 },
+  { id: "pete", name: "Pete Peterson", age: 31 },
 ];
 
 function groupById(array) {
   //write your code here
+  return array.reduce((val1, val2) => {
+    let arrayValue = val2.id;
+    val1[arrayValue] = val2;
+    return val1;
+  }, {});
 }
 
-let usersById= groupById(users)
-console.log(usersById)
+let usersById = groupById(users);
+console.log(usersById);
 
 /* expected output: 
 
